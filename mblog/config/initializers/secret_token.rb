94aba0909d3fdd70818d46bb9905c69ee31375b9
Nickle <nickle-github@nickle.ath.cx>
2012-03-05 21:52:43 +0900

    rails new mblog -d postgresql

---
+++ mblog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mblog::Application.config.secret_token = 'd3dc5399e68d5dd4af9ef6e8d6cdbac3b4814ac477e1313a5800782add83b28310d59b0ed41ad3c084afe60d2300beddf4e0cc1d5b0b3492d52888726db966eb'
