47fda3b74145c2ed99b660524b6c141805317b9a
Nickle <nickle-github@nickle.ath.cx>
2012-03-06 23:00:42 +0900

    bootstrapを使ってそこそこのデザインに整えた

--- mblog/config/application.rb
+++ mblog/config/application.rb
@@ -55,5 +55,7 @@ module Mblog
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.paths << Rails.root.join("vendor", "assets", "images")
   end
 end
