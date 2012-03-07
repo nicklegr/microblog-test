b906fb170b8f90e98d87bf577673d7da6096896e
Nickle <nickle-github@nickle.ath.cx>
2012-03-07 23:46:16 +0900

    同名のユーザも作れないようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 
 class User < ActiveRecord::Base
-  validates_presence_of :name, :message => 'ユーザー名が空です'
+  validates_presence_of :name, :message => 'ユーザ名が空です'
+  validates_uniqueness_of :name, :message => '既に同名のユーザが存在します'
 end
